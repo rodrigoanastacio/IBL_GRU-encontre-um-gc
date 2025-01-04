ad064f974273e96de9c7ab714c85c46308a5d4ac
Rodrigo Anastacio <rodrigo.anastacio@dtidigital.com.br>
2025-01-04 14:23:53 -0300

    feat: add pagina busca gc

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,11 +3,13 @@ import { Home } from "../pages/Home";
 import { DashboardLayout } from "../layouts/DashboardLayout";
 import { Dashboard } from "../pages/Dashboard";
 import { GCList } from "../pages/Dashboard/GCList";
+import { NearestGC } from "../pages/NearestGC";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/encontrar-gc" element={<NearestGC />} />
       <Route path="/dashboard" element={<DashboardLayout />}>
         <Route index element={<Dashboard />} />
         <Route path="gcs" element={<GCList />} />
